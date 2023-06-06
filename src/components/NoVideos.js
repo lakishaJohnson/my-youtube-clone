5787a2a45265721e10ccd480ca948f4c2c5a4393
Lakisha J <lakishajohnson@pursuit.org>
2023-06-06 12:04:55 -0400

    No video message component

---
+++ src/components/NoVideos.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./NoVideos.css";
+
+function NoVideos() {
+  return (
+    <div className="no-videos-container">
+      <img
+        src="https://wehearthart.co.uk/wp-content/uploads/2018/11/Move-along-nothing-to-see-here.jpg"
+        alt="No videos"
+        className="image"
+      />
+    </div>
+  );
+}
+
+export default NoVideos;
